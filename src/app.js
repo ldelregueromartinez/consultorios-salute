52c549761e46f69bba0c91d2fbe892e7d7b22a81
Lucas del Reguero Martinez <ldelregueromartinez@siu.edu.ar>
2023-10-22 20:20:18 -0300

    avance2

--- src/app.js
+++ src/app.js
@@ -6,20 +6,20 @@ const app = express();
 // Importar 
 
 import especialistasRoutes from './routes/especialistas.routes.js';
-//import historiasclinicasRoutes from './routes/historiasclinicas.routes.js';
+import historiasclinicasRoutes from './routes/historiasclinicas.routes.js';
 import pacientesRoutes from './routes/pacientes.routes.js';
 import consultoriosRoutes from './routes/consultorios.routes.js';
-//import asientosclinicos from './asientosclinicos.routes.js/';
+import asientosclinicos from './asientosclinicos.routes.js/';
 
 // Midlewares
 app.use(express.json());
 
 //Rutas
 app.use(especialistasRoutes);
-//app.use(historiasclinicasRoutes);
+app.use(historiasclinicasRoutes);
 app.use(pacientesRoutes);
 app.use(consultoriosRoutes);
-//app.use(asientosclinicos);
+app.use(asientosclinicos);
 
 
 // exportar el servidor
