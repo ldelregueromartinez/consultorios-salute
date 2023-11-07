5041088a5dcbae75909b1191a412055d05eb568f
Lucas del Reguero Martinez <ldelregueromartinez@siu.edu.ar>
2023-11-06 22:47:27 -0300

    inicio

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,6 +12,15 @@ import consultoriosRoutes from './routes/consultorios.routes.js';
 import asientosRoutes from './routes/asientos.routes.js';
 
 // Midlewares
+
+app.use((req,res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
 app.use(express.json());
 
 //Rutas
